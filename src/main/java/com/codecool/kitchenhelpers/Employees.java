4fabbe2f1ab0c929520bcea0f274b0568209edee
DDubicki <dubickidymitr@gmail.com>
2021-10-22 18:01:53 +0200

    Starter pack

---
+++ src/main/java/com/codecool/kitchenhelpers/Employees.java
@@ -0,0 +1,21 @@
+package com.codecool.kitchenhelpers;
+
+import java.util.Date;
+
+public class Employees {
+    private String name;
+    private Date BirthDate;
+    private double salary;
+
+    public String getName() {
+        return name;
+    }
+
+    public Date getBirthDate() {
+        return BirthDate;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+}
